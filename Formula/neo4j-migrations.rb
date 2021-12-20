e239c3b7524b154deeaac83ff02fb1e7b47106f9
jreleaserbot <jreleaser@kordamp.org>
2021-12-20 10:12:21 +0000

    neo4j-migrations 1.2.2

--- Formula/neo4j-migrations.rb
+++ Formula/neo4j-migrations.rb
@@ -1,19 +1,22 @@
 class Neo4jMigrations < Formula
   desc "Simple, Flyway DB inspired migrations for Neo4j."
   homepage "https://github.com/michael-simons/neo4j-migrations"
-  url "https://github.com/michael-simons/neo4j-migrations/releases/download/1.2.1/neo4j-migrations-1.2.1-osx-x86_64.zip"
-  version "1.2.1"
-  sha256 "625e8fe59aa5ac53b42eaf3898ab9f27a2f02b049c1d1ab8c0fded6918b144c3"
+  url "https://github.com/michael-simons/neo4j-migrations/releases/download/1.2.2/neo4j-migrations-1.2.2-osx-x86_64.zip"
+  version "1.2.2"
+  sha256 "95480e05f6e13687f7b83ba3de09fcc01d8305a50c1fa0efea4520c9375144cf"
   license "The Apache Software License, Version 2.0"
 
 
   def install
+    bash_completion.install "completion/neo4j-migrations_completion" => "neo4j-migrations"
+    zsh_completion.install "completion/zsh/_neo4j-migrations" => "_neo4j-migrations"
+
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/neo4j-migrations"
   end
 
   test do
     output = shell_output("#{bin}/neo4j-migrations --version")
-    assert_match "1.2.1", output
+    assert_match "neo4j-migrations/1.2.2", output
   end
 end
