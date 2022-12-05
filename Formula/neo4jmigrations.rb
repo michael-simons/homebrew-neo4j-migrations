9edbc828bc1734627eee496681521a6fc35410af
jreleaserbot <jreleaser@kordamp.org>
2022-12-05 09:08:33 +0000

    Bump neo4j-migrations to 1.15.1.

--- Formula/neo4jmigrations.rb
+++ Formula/neo4jmigrations.rb
@@ -1,9 +1,9 @@
 class Neo4jMigrations < Formula
   desc "Simple, Flyway DB inspired migrations for Neo4j."
   homepage "https://github.com/michael-simons/neo4j-migrations"
-  url "https://github.com/michael-simons/neo4j-migrations/releases/download/1.15.0/neo4j-migrations-1.15.0-osx-x86_64.zip"
-  version "1.15.0"
-  sha256 "0d4309dae8f16f072c0ccfc5d38c4b90fad9b7f77baace89037a36978d35df8b"
+  url "https://github.com/michael-simons/neo4j-migrations/releases/download/1.15.1/neo4j-migrations-1.15.1-osx-x86_64.zip"
+  version "1.15.1"
+  sha256 "486ce985f308d5b7637872492286a3054b155713a56d09b6966e414155de7a24"
   license "The Apache Software License, Version 2.0"
 
 
@@ -17,6 +17,6 @@ class Neo4jMigrations < Formula
 
   test do
     output = shell_output("#{bin}/neo4j-migrations --version")
-    assert_match "neo4j-migrations/1.15.0", output
+    assert_match "neo4j-migrations/1.15.1", output
   end
 end
