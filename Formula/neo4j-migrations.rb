1066ae2bdf3bbfde367507a237bd5e5590a22708
Oriol del Rio <contact@odelrio.com>
2023-12-15 00:31:11 +0100

    fix: Restore hyphen in formula filename

--- Formula/neo4j-migrations.rb
+++ Formula/neo4j-migrations.rb
@@ -1,9 +1,9 @@
 class Neo4jMigrations < Formula
   desc "Simple, Flyway DB inspired migrations for Neo4j."
   homepage "https://github.com/michael-simons/neo4j-migrations"
-  url "https://github.com/michael-simons/neo4j-migrations/releases/download/1.13.2/neo4j-migrations-1.13.2-osx-x86_64.zip"
-  version "1.13.2"
-  sha256 "3a92030774fa8e29aa9f07acbb1bf247b9c8c972a744deab866d15aa22f24870"
+  url "https://github.com/michael-simons/neo4j-migrations/releases/download/2.8.0/neo4j-migrations-2.8.0-osx-x86_64.zip"
+  version "2.8.0"
+  sha256 "8a55fce8c8b836764c94e9ef65880f4b3049c0b33a97190e9bfd3f347c7c2116"
   license "The Apache Software License, Version 2.0"
 
 
@@ -17,6 +17,6 @@ class Neo4jMigrations < Formula
 
   test do
     output = shell_output("#{bin}/neo4j-migrations --version")
-    assert_match "neo4j-migrations/1.13.2", output
+    assert_match "neo4j-migrations/2.8.0", output
   end
 end
